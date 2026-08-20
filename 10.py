727bc81ab3ad74d4405af3bfe414f4e97fcbd567
sohamwork27-netizen <sohamwork27@gmail.com>
2026-08-20 19:54:22 +0530

    Add script to sort and print a list of names

---
+++ 10.py
@@ -0,0 +1,5 @@
+names = ["Aarav Sharma", "Riya Patil", "Aditya Kulkarni", "Sneha Deshmukh", "Rohit Joshi", "Ananya Mehta", "Kunal Shah", "Pooja Chavan", "Siddharth More", "Neha Pawar"]
+
+names.sort()
+
+print(names)
